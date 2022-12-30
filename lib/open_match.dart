9b68392d8a38fd32800b2721b0d76ef9491d70e7
markvideon <mark.videon@gmail.com>
2022-12-30 19:28:02 +1030

    Added additional exports from main library file.

--- lib/open_match.dart
+++ lib/open_match.dart
@@ -3,10 +3,42 @@
 /// scalable and extensible Matchmaker.
 library open_match;
 
+// Backend
 export 'generated/api/backend.pb.dart';
+export 'generated/api/backend.pbenum.dart';
+export 'generated/api/backend.pbgrpc.dart';
+export 'generated/api/backend.pbjson.dart';
+
+// Evaluator
 export 'generated/api/evaluator.pb.dart';
+export 'generated/api/evaluator.pbenum.dart';
+export 'generated/api/evaluator.pbgrpc.dart';
+export 'generated/api/evaluator.pbjson.dart';
+
+// Extensions
 export 'generated/api/extensions.pb.dart';
+export 'generated/api/extensions.pbenum.dart';
+export 'generated/api/extensions.pbjson.dart';
+
+// Frontend
 export 'generated/api/frontend.pb.dart';
+export 'generated/api/frontend.pbenum.dart';
+export 'generated/api/frontend.pbgrpc.dart';
+export 'generated/api/frontend.pbjson.dart';
+
+// Match Function
 export 'generated/api/matchfunction.pb.dart';
+export 'generated/api/matchfunction.pbenum.dart';
+export 'generated/api/matchfunction.pbgrpc.dart';
+export 'generated/api/matchfunction.pbjson.dart';
+
+// Messages
 export 'generated/api/messages.pb.dart';
+export 'generated/api/messages.pbenum.dart';
+export 'generated/api/messages.pbjson.dart';
+
+// Query
 export 'generated/api/query.pb.dart';
+export 'generated/api/query.pbenum.dart';
+export 'generated/api/query.pbgrpc.dart';
+export 'generated/api/query.pbjson.dart';
